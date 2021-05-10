3b076e888827b001d5ad0784e88b3cbb39c4484e
Robby Balistreri <rbalistreri5@gmail.com>
2021-05-10 17:57:27 -0400

    begin modal creation

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,12 +1,15 @@
 import EntitiesReducer from './entities_reducer'
 import ErrorsReducer from './errors_reducer'
 import SessionReducer from './session_reducer'
+import UiReducer from './ui_reducer'
 import {combineReducers} from 'redux'
 
 const RootReducer = combineReducers({
     entities: EntitiesReducer,
     errors: ErrorsReducer,
-    session: SessionReducer
+    session: SessionReducer,
+    ui: UiReducer
+
 })
 
 export default RootReducer;
---
+++ frontend/reducers/ui_reducer.js
@@ -0,0 +1,8 @@
+import ModalReducer from './modal_reducer';
+import {combineReducers} from 'redux'
+
+const UiReducer = combineReducers({
+    modal: ModalReducer
+})
+
+export default UiReducer;
