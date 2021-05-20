1e57926efc6a280e4fa1e077122312dba7908f57
Robby Balistreri <rbalistreri5@gmail.com>
2021-05-20 15:58:05 -0500

    Add review averages to the header

--- frontend/components/property_page/property_header.jsx
+++ frontend/components/property_page/property_header.jsx
@@ -10,13 +10,16 @@ class PropertyHeader extends React.Component {
     const 
 
     render() {
-        
+        let total = 0;
+        Object.values(this.props.currentProperty.reviews).forEach(rev => total += rev.rating)
+        let number = Object.values(this.props.currentProperty.reviews).length
+        let average = (total / number).toFixed(2)
         return (
             <div className='property-header'>
                 <h2 className='property-header-name'>{this.props.currentProperty.property.name}</h2>
                 <div className='property-header-bar'>
                     <div className='property-header-subitems'>
-                        <p>{Object.values(this.props.currentProperty.reviews).length} Total Reviews</p>
+                        <p><i className='fa fa-star review-stars'></i> {average} ({number} reviews)</p>
                         <p>Superhost</p>
                         <p>{this.props.city.name}, {this.props.state.name}</p>    
                     </div>
